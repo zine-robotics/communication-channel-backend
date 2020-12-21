615c67c84b89c15435ea0618a07eb7756f79c539
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2020-12-21 11:43:44 +0530

    added join room route

--- controllers/chat.js
+++ controllers/chat.js
@@ -3,7 +3,7 @@ const Conversation = require("../models/conversation");
 exports.createRoom = (req, res) => {
   if (!req.body) {
     return res
-      .status(200)
+      .status(400)
       .json({ mesaage: "Invalid request to create chat room" });
   }
   const { conversationName } = req.body;
@@ -17,5 +17,41 @@ exports.createRoom = (req, res) => {
     if (data) {
       res.status(200).json({ message: "Succesfully created chatroom" });
     }
+  });
+};
+
+exports.joinRoom = (req, res) => {
+  if (!req.body) {
+    return res
+      .status(400)
+      .json({ message: "Invalid request to join chatroom" });
+  }
+  const { roomId, userId } = req.body;
+  Conversation.findOne({ _id: roomId }).exec((error, room) => {
+    if (error) return res.status(400).json({ error });
+    if (room) {
+      userExists = room.participants.find((u) => u.id == userId);
+      if (userExists) {
+        return res
+          .status(400)
+          .json({ message: "User already exists in that chatroom" });
+      } else {
+        const condition = { _id: roomId };
+        const update = {
+          $push: {
+            participants: { id: userId },
+          },
+        };
+        Conversation.findOneAndUpdate(condition, update).exec(
+          (error, _room) => {
+            if (error) return res.status(400).json({ error });
+            if (_cart) return res.status(200).json({ room: _room });
+          }
+        );
+      }
+    } else
+      return res
+        .status(400)
+        .json({ message: "No room exist. Please create room first." });
   });
 };
--- routes/chats.js
+++ routes/chats.js
@@ -1,9 +1,11 @@
-const { createRoom } = require("../controllers/chat");
+const { createRoom, joinRoom } = require("../controllers/chat");
 const { requireSignin } = require("../middlewares");
+const Conversation = require("../models/conversation");
 
 express = require("express");
 const router = express.Router();
 
 router.post("/createChatRoom", requireSignin, createRoom);
+router.post("/joinChatRoom", requireSignin, joinRoom);
 
 module.exports = router;
