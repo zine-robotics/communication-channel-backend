5df5bf9a8441a64ad897f2748126ae6bd810d6bc
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2020-12-22 23:13:40 +0530

    add get rooms route

--- routes/chats.js
+++ routes/chats.js
@@ -7,5 +7,19 @@ const router = express.Router();
 
 router.post("/createroom", requireSignin, createRoom);
 router.post("/joinroom", requireSignin, joinRoom);
+router.get("/rooms", requireSignin, (req, res) => {
+  if (!req.body) {
+    return res
+      .status(400)
+      .json({ mesaage: "Invalid request to create chat room" });
+  }
+  const userId = req.body.userId;
+  const chats = Conversation.find({"participants.id": userId})
+  if(!chats) {
+      return res.status(400).json({message: "Couldn't find any chats for that user"})
+  } else {
+      return res.status(200).json({chats})
+  }
+});
 
 module.exports = router;
