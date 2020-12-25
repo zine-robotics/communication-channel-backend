645d19c9116f5e4179f117a0408e6fd267a4cfdd
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2020-12-25 23:37:53 +0530

    fix crashing while signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,41 +3,46 @@ const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res) => {
   User.findOne({ email: req.body.email }).exec((error, user) => {
+    if (error) {
+      console.log(error);
+    }
+
     if (user)
       return res
         .status(400)
         .json({ message: "User with that email already exist" });
-  });
+    else {
+      const {
+        firstName,
+        lastName,
+        email,
+        password,
+        rollNumber,
+        college,
+        domainOfInterest,
+      } = req.body;
 
-  const {
-    firstName,
-    lastName,
-    email,
-    password,
-    rollNumber,
-    college,
-    domainOfInterest,
-  } = req.body;
+      const _user = new User({
+        firstName,
+        lastName,
+        email,
+        password,
+        rollNumber,
+        college,
+        domainOfInterest,
+      });
 
-  const _user = new User({
-    firstName,
-    lastName,
-    email,
-    password,
-    rollNumber,
-    college,
-    domainOfInterest,
-  });
+      _user.save((error, data) => {
+        if (error) {
+          console.log(error);
+          return res.status(400).json({ error: "Something went wrong" });
+        }
 
-  _user.save((error, data) => {
-    if (error) {
-      console.log(error);
-      return res.status(400).json({ error: "Something went wrong" });
-    }
-
-    if (data) {
-      return res.status(200).json({
-        message: "Registration Successful",
+        if (data) {
+          return res.status(200).json({
+            message: "Registration Successful",
+          });
+        }
       });
     }
   });
