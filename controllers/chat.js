1a6b305874459e1646a109c80d409bdbdb45566b
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2020-12-25 16:00:11 +0530

    fixed bug after testing

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,13 +1,14 @@
 const Conversation = require("../models/conversation");
 
 exports.createRoom = (req, res) => {
+  console.log(req.body);
   if (!req.body) {
     return res
       .status(400)
       .json({ mesaage: "Invalid request to create chat room" });
   }
   const { conversationName } = req.body;
-  _chatRoom = new Conversation(conversationName);
+  _chatRoom = new Conversation({ conversationName });
   _chatRoom.save((error, data) => {
     if (error) {
       console.log(error);
@@ -45,7 +46,7 @@ exports.joinRoom = (req, res) => {
         Conversation.findOneAndUpdate(condition, update).exec(
           (error, _room) => {
             if (error) return res.status(400).json({ error });
-            if (_cart) return res.status(200).json({ room: _room });
+            if (_room) return res.status(200).json({ room: _room });
           }
         );
       }
@@ -54,4 +55,21 @@ exports.joinRoom = (req, res) => {
         .status(400)
         .json({ message: "No room exist. Please create room first." });
   });
+};
+
+exports.getRooms = async (req, res) => {
+  if (!req.body) {
+    return res
+      .status(400)
+      .json({ mesaage: "Invalid request to create chat room" });
+  }
+  const userId = req.body.userId;
+  const chats = await Conversation.find({ "participants.id": userId });
+  if (!chats) {
+    return res
+      .status(400)
+      .json({ message: "Couldn't find any chats for that user" });
+  } else {
+    return res.status(200).json({ chats });
+  }
 };
--- routes/chats.js
+++ routes/chats.js
@@ -1,4 +1,4 @@
-const { createRoom, joinRoom } = require("../controllers/chat");
+const { createRoom, joinRoom, getRooms } = require("../controllers/chat");
 const { requireSignin } = require("../middlewares");
 const Conversation = require("../models/conversation");
 
@@ -7,19 +7,6 @@ const router = express.Router();
 
 router.post("/createroom", requireSignin, createRoom);
 router.post("/joinroom", requireSignin, joinRoom);
-router.get("/rooms", requireSignin, (req, res) => {
-  if (!req.body) {
-    return res
-      .status(400)
-      .json({ mesaage: "Invalid request to create chat room" });
-  }
-  const userId = req.body.userId;
-  const chats = Conversation.find({"participants.id": userId})
-  if(!chats) {
-      return res.status(400).json({message: "Couldn't find any chats for that user"})
-  } else {
-      return res.status(200).json({chats})
-  }
-});
+router.get("/rooms", requireSignin, getRooms);
 
 module.exports = router;
