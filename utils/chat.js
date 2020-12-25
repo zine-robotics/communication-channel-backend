09fef73ad74e9e2286aa333043cd0d560e7440ed
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2020-12-25 21:54:51 +0530

    added rtc part

---
+++ utils/chat.js
@@ -0,0 +1,53 @@
+const Message = require("../models/message");
+const users = [];
+
+// Join user to chat
+function userJoin(id, username, room) {
+  const user = { id, username, room };
+
+  users.push(user);
+
+  return user;
+}
+
+// Get current user
+function getCurrentUser(id) {
+  return users.find((user) => user.id === id);
+}
+
+// User leaves chat
+function userLeave(id) {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+// Get room users
+function getRoomUsers(room) {
+  return users.filter((user) => user.room === room);
+}
+
+// function findUserFromDb(userId) {
+//     const user = User.find.findOne({_id: })
+// }
+function createNewMessageInDb(userId, message, roomId) {
+  const _message = new Message({
+    senderId: userId,
+    content: message,
+    conversationId: roomId,
+  });
+  _message.save((error, data) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+}
+module.exports = {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+  createNewMessageInDb,
+};
