1a50a59416c371bfeafe17cc773b1e5f99e6f1a4
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-01-11 21:01:38 +0530

    fix bugs

--- controllers/info.js
+++ controllers/info.js
@@ -9,7 +9,18 @@ exports.userInfo = async (req, res) => {
       if (!user) {
         return res.status(400).json({ message: "No user exists" });
       } else {
-        return res.status(200).json({ user });
+        return res.status(200).json({
+          user: {
+            role: user.role,
+            college: user.college,
+            domainOfInterest: user.domainOfInterest,
+            _id: user._id,
+            fullName: user.fullName,
+            rollNumber: user.rollNumber,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
+          },
+        });
       }
     })
     .catch((error) => {
