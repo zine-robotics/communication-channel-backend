403201c3d413e6b8cafbc329ebf085a30c5123dd
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-02-04 16:00:47 +0530

    remove log from auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,6 @@ const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res) => {
-  console.log(req);
   User.findOne({ email: req.body.email }).exec((error, user) => {
     if (error) {
       console.log(error);
